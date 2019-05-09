b2f24ee40ff9a4dd110c6c68bbf629988a48c97e
BuildTools <unconfigured@null.spigotmc.org>
2019-05-10 00:17:20 +1000

    zzz

--- master/main.rb
+++ master/main.rb
@@ -4,14 +4,35 @@ require './object'
 require './player'
 
 WIDTH = 1000
-HEIGHT = 500
+HEIGHT = 501
 
-def get_random_spawn_location(object)
-	last_obj = object[object.length-1].y
-	loop do
-		seed = rand(HEIGHT)
-		return seed if seed < HEIGHT-100 and seed > 100 and seed + last_obj > last_obj or seed + last_obj < last_obj
+def peek_array(array)
+	if array[array.length-1]
+		return array[array.length-1]
+	else
+		return nil
 	end
+
+end
+
+def get_random_spawn_location(type)
+	object = peek_array(@list_of_objs[type])
+
+	if object
+		loop do
+			seed = rand(HEIGHT)
+			# return seed if seed < HEIGHT and seed > 0 and seed > last_obj_height and seed < last_obj_y
+			return seed if seed < object.y-100 or seed > object.h*object.scale+100 and seed < HEIGHT-100 and seed > 100
+		end
+	else
+		return 100
+	end
+
+		# loop do
+		# 	seed = rand(HEIGHT)
+		# 	# return seed if seed < HEIGHT and seed > 0 and seed > last_obj_height and seed < last_obj_y
+		# 	return seed if seed > last_obj_y-50 or seed < last_obj_height+50 and seed < HEIGHT-100 and seed > 100
+		# end
 
 end
 
@@ -20,22 +41,20 @@ class GameWindow < Gosu::Window
             super WIDTH, HEIGHT
             self.caption = "Game"
 
-            # takes spawn_x, spawn_y, source, width, height
-            # @cloud = Obs.new(0, 240, './media/cloud1.png', 846, 540)
 		@clouds = Array.new()
-		@clouds << @cloud = spawn_obj(-1, rand(HEIGHT-100), './media/cloud1.png', 846, 540, 4, 0, 0.3, 1, true)
+		@clouds << cloud = spawn_obj(-50, 100, './media/cloud1.png', 846, 540, 4, 0, 0.3, 1, true)
 
-
-
-            # @cloud2 = spawn_obj(0, 240, './media/cloud1.png', 846, 540, 4, 0, 0.3, 1)
-		@background = spawn_obj(0, 0, './media/background.png', WIDTH, HEIGHT, 0, 0, 1, 0, false)
-
-            # @player = Player.spawn_player(100, 240, './media/hook.png', 50, 50)
+		# @background = spawn_obj(0, 0, './media/background.png', WIDTH, HEIGHT, 0, 0, 1, 0, false)
 
             @key = {
                   kb_left: Gosu::KbLeft,
                   kb_right: Gosu::KbRight,
             }
+
+		@list_of_objs = {
+			# frames, and time in secs
+			:cloud => @clouds
+		}
       end
 
       # runs before update
@@ -48,43 +67,26 @@ class GameWindow < Gosu::Window
       # update
       def update
 		if rand(100) == 1
-			r = get_random_spawn_location(@clouds)
-			@clouds << @cloud = spawn_obj(-1, get_random_spawn_location(@clouds), './media/cloud1.png', 846, 540, 4, 0, 0.3, 1, true)
-			puts "spawned cloud #{r}"
+			puts "new cloud"
+			@clouds << cloud = spawn_obj(-50, get_random_spawn_location(:cloud), './media/cloud1.png', 846, 540, 4, 0, 0.3, 1, false)
 		end
 
 		@clouds.each do |cloud|
-			if @clouds.length > 3
-				@clouds[0].wrapping = false
-			end
-			update_object(cloud, 4, 0)
+			update_object(cloud, cloud.velx, cloud.vely)
 		end
       end
 
 	#draw
 	def draw
-		@clouds.each do |cloud|
-			draw_obj(cloud, :right)
+		if @clouds
+			@clouds.each do |cloud|
+				draw_obj(cloud, :right)
+			end
 		end
 
 		draw_obj(@background, :right)
       end
 
 end
-
-# class Animation
-# 	attr_accessor: x, y, frames, ms_per_frame, current_frame
-# end
-#
-# def update_animation(anim_to_update)
-# 	anim_to_update.current_frame += anim_to_update.ms_per_frame
-# 	if (anim_to_update.current_frame > anim_to_update.length)
-#
-# end
-#
-# def draw_animation(anim_to_draw)
-# 	current_frame = math.floor(anim_to_draw.current_frame)
-# 	Gosu.draw(x, y, anim_to_draw.frames[current_frame])
-# end
 
 GameWindow.new.show
--- master/object.rb
+++ master/object.rb
@@ -2,21 +2,39 @@ require 'gosu'
 require './animation'
 
 class Obj
-	attr_accessor :x, :y, :frames, :scale, :zaxis, :wrapping
-	def initialize(x, y, frames, w, h, velx, vely, scale, zaxis, wrapping)
-		@frames = frames
+	attr_accessor :x, :y, :w, :h, :tiles, :keyframes, :scale, :zaxis, :wrapping, :velx, :vely
+	def initialize(x, y, tiles, w, h, velx, vely, scale, zaxis, wrapping)
 		@x, @y = x, y
 		@velx = velx
 		@vely = vely
 		@scale = scale
 		@zaxis = zaxis
 		@wrapping = wrapping
+		@h = h
+		@w = w
+		@tiles = tiles
+		# instance a new animation
+		@keyframes = get_keyframes(self)
 	end
 end
 
 def spawn_obj(x, y, spritesheet, w, h, velx, vely, scale, zaxis, wrapping)
-	frames = Gosu::Image.load_tiles(spritesheet, w, h)
-	Obj.new(x, y, frames, w, h, velx, vely, scale, zaxis, wrapping)
+	puts "spawning object #{spritesheet}"
+	tiles = Gosu::Image.load_tiles(spritesheet, w, h)
+	Obj.new(x, y, tiles, w, h, velx, vely, scale, zaxis, wrapping)
+end
+
+# reads all of the keyframes for all sprites of a particular object
+def get_keyframes(object)
+	object.tiles.length > 0 ? (last_frame = object.tiles.length) : (last_frame = 1)
+	
+	if object.tiles.length > 0
+		keyframes = {
+			:right => Animation.new(object.tiles[0..last_frame], 0.4)
+		}
+	end
+
+	return keyframes
 end
 
 
@@ -30,7 +48,7 @@ end
 
 # determines if an object needs to wrap the screen
 def process_boundaries(object)
-	if object.x > 1000 or object.x < -300
+	if object.x > WIDTH+300 or object.x < -300
 		if !object.wrapping
 			@clouds.delete_at(0)
 			puts "deleted cloud"
@@ -40,19 +58,10 @@ def process_boundaries(object)
 	end
 end
 
-# reads all of the keyframes for all sprites
-def get_keyframes(object)
-	keyframes = {
-		# 0..4 indicates the frames. 0.x indicates anim speed
-		:right => Animation.new(object.frames[0..object.frames.length], 0.4),
-		:left => Animation.new(object.frames[0..object.frames.length], 0.4)
-	}
-	return keyframes
-end
+
 
 def draw_obj(object, direction)
-	if object.frames
-		keyframes = get_keyframes(object)
-		keyframes[direction].start.draw(object.x, object.y, object.zaxis, object.scale , object.scale)
+	if object
+		object.keyframes[direction].start.draw(object.x, object.y, object.zaxis, object.scale , object.scale)
 	end
 end
