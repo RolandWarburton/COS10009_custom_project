f107b29f105108b0154a43a89eec8d2a62dc5f41
Roland <warburtonroland@gmail.com>
2019-05-10 18:44:43 +1000

    fixed cols and rows in cell generation

--- master/map.rb
+++ master/map.rb
@@ -11,23 +11,26 @@ end
 
 def generate_cells()
 
-      cols = CELL_Y_COUNT
-      rows = CELL_X_COUNT
+      cols = CELL_X_COUNT
+      rows = CELL_Y_COUNT
       table = Array.new(rows)
       items = [0,-1]
 
-
-      table.each_with_index do |col, row_index|
+	row_index = 0
+      while row_index < rows
             table[row_index] = Array.new(2)
             col_index = 0
             while col_index < cols
-			block = spawn_obj(0, 0, './media/dirt.png', 50, 50, 0, 0, 1, 0, false)
-			block.x = row_index*CELL_DIM
-			block.y = col_index*CELL_DIM
+			row_index < 3 ? (media = "./media/air.png") : (media = "./media/dirt.png")
+			block = spawn_obj(0, 0, media, 50, 50, 0, 0, 1, 0, false)
+			block.x = col_index*CELL_DIM
+			block.y = row_index*CELL_DIM
+			p "#{row_index},#{col_index}"
 
                   table[row_index][col_index] = Node.new(row_index, col_index, block)
                   col_index += 1
             end
+		row_index += 1
       end
 
       return table
